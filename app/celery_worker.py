1b1b0744890eedc0a0488f9eaa6ead76be6c2a83
eduardo almada <almada.eduardo@gmail.com>
2025-07-07 23:12:18 -0300

    Update celery_worker.py
    add o código no final
    
    from celery.schedules import crontab
    
    celery.conf.beat_schedule = {
        'buscar-publicacoes-diariamente': {
            'task': 'app.tasks.tarefa_buscar_publicacoes',
            'schedule': crontab(hour=8, minute=0),  # ⏰ 08:00 da manhã todos os dias
        },
    }

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -14,3 +14,12 @@ celery.autodiscover_tasks(['app'])
 
 # Força o registro das tasks (sem causar import circular)
 import app.tasks  # noqa
+
+from celery.schedules import crontab
+
+celery.conf.beat_schedule = {
+    'buscar-publicacoes-diariamente': {
+        'task': 'app.tasks.tarefa_buscar_publicacoes',
+        'schedule': crontab(hour=8, minute=0),  # ⏰ 08:00 da manhã todos os dias
+    },
+}
