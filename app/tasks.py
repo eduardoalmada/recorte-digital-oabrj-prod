a362b3a9e7f292d505c1ed3b96414ea78107468e
eduardo almada <almada.eduardo@gmail.com>
2025-07-07 22:47:29 -0300

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -2,5 +2,5 @@ from celery import shared_task
 
 @shared_task
 def tarefa_buscar_publicacoes():
-    print("🔎 Buscando publicações no DataJud...")
-    # Aqui vai a lógica da busca real
+    print("🔎 Iniciando busca de publicações no DataJud...")
+    # Lógica real da busca vai aqui
