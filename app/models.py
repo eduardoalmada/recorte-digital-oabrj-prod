533312ad9c4a4ed813ea847be0d90bed548f94b9
eduardo almada <almada.eduardo@gmail.com>
2025-07-08 23:07:00 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 class Advogado(db.Model):
     __tablename__ = 'advogado'  # define explicitamente o nome da tabela
