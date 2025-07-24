49f5d354192f19a3c7927a6e95b423e43f1d3e73
Eduardo Almada <almada.eduardo@gmail.com>
2025-07-24 13:59:38 -0300

    Conecta tarefa Celery ao scraper do DJERJ

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
+from app.scrapers.scraper_djerj_selenium import processar_publicacoes_djerj
 
 @shared_task
 def tarefa_buscar_publicacoes():
-    print("🔎 Iniciando busca de publicações no DataJud...")
-    # Lógica real da busca vai aqui
+    print("🔎 Iniciando busca de publicações no DJERJ...")
+    processar_publicacoes_djerj()
