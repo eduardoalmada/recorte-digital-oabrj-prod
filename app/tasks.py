a642a1282927eabce93bf5b8cb85a458e35d53e8
eduardo almada <almada.eduardo@gmail.com>
2025-07-30 12:05:10 -0300

    Update tasks.py
    from app.scrapers.scraper_djerj_selenium import processar_publicacoes_djerj
    from app import create_app
    from app.celery_worker import celery

--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,10 @@
-from celery import shared_task
 from app.scrapers.scraper_djerj_selenium import processar_publicacoes_djerj
+from app import create_app
+from app.celery_worker import celery
 
-@shared_task
+@celery.task(name="app.tasks.tarefa_buscar_publicacoes")
 def tarefa_buscar_publicacoes():
     print("🔎 Iniciando busca de publicações no DJERJ...")
-    processar_publicacoes_djerj()
+    app = create_app()
+    with app.app_context():
+        processar_publicacoes_djerj()
