dadf97e3b070bd43a59f74737d3e280c4a89c561
eduardo almada <almada.eduardo@gmail.com>
2025-07-30 12:32:11 -0300

    Update scraper_djerj_selenium.py
    Chrome headless de forma compatível com o ambiente do Render

--- app/scrapers/scraper_djerj_selenium.py
+++ app/scrapers/scraper_djerj_selenium.py
@@ -2,7 +2,6 @@ import time
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import requests
 
@@ -12,14 +11,13 @@ from app.models import Advogado, Publicacao
 app = create_app()
 
 def configurar_driver():
-    options = Options()
-    options.binary_location = "/usr/bin/google-chrome"
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920x1080")
-    return webdriver.Chrome(service=Service(), options=options)
+    options.add_argument("--no-sandbox")
+    options.binary_location = "/usr/bin/google-chrome"
+
+    return webdriver.Chrome(options=options)
 
 def buscar_publicacoes_djerj():
     hoje = datetime.now().strftime("%d/%m/%Y")
