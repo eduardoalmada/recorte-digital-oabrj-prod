05e63174fc217f5f7c71d80cc3ddce5338645896
eduardo almada <almada.eduardo@gmail.com>
2025-08-20 15:09:47 -0300

    Update main.py
    atualização

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Em produção, o Gunicorn usa "main:app"
+    app.run(host="0.0.0.0", port=5000, debug=False)
